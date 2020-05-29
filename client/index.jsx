015f43831fb1fc541dca2b3c239af5d966c23387
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-05-28 21:23:24 -0400

    added scss files

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App.jsx';
 
-// uncomment so that webpack can bundle styles
-// import styles from './scss/application.scss';
+import styles from './scss/main.scss';
 
 render(<App />, document.getElementById('root'));
