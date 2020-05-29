cfc6889e0f9e7224b286015d9728a2b0817214db
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-05-28 20:56:11 -0400

    added app.jsx and react

---
+++ client/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>NICE</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
