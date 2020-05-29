7a98612be48168024b65eb647bc799dd5d301efa
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-05-28 21:02:42 -0400

    initialized webpack.config.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '../client/')));
+
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
