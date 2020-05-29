4f0caee75028449d40510a010b97f5819c43ffe1
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-05-28 20:40:54 -0400

    added basic express server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
