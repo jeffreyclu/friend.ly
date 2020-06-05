1380d24fd7bd54b7075040613019b458e179b9b1
Jeffrey Lu <yumyumyum@yumyumyum-mothership.local>
2020-06-05 00:05:43 -0400

    built

--- client/components/ChatroomApp.jsx
+++ client/components/ChatroomApp.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ChatMessage from './ChatMessage.jsx';
 
 const ChatroomApp = ({ messages, participants, setNewMessage, sendMessage, user }) => {
-  const participantList = participants.map((name, index) => <h3>{name}</h3>);
   const messageList = messages.map((chat, index) => <ChatMessage key={`${index} ${chat.createdAt}`}chat={chat} user={user} />)
   return (
     <div className="chatroom">
-      <h3>Chatroom: {participants[0] + " - " + participants[1]}</h3>
+      <h3>{ user.name === participants[0] ? participants[1] : participants[0] }</h3>
       <div className="chatMessageArea" id="chatArea">
         { messages.length > 0 ? messageList : <h4>It's empty in here... why don't you break the ice?</h4> }
       </div>
